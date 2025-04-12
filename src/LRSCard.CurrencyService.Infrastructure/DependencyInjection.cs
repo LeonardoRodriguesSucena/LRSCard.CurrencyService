f94f524214649c54271e20e4901bb36a3e4777f9
Leonardo Rodrigues Sucena <leonardo.sucena@gmail.com>
2025-04-12 19:20:27 +0400

    + adding Resilience options for Exchange Providers

--- src/LRSCard.CurrencyService.Infrastructure/DependencyInjection.cs
+++ src/LRSCard.CurrencyService.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Extensions.Http;
-using LRSCard.CurrencyService.Application.Interfaces;
-using LRSCard.CurrencyService.Infrastructure.ExchangeRateProviders.Frankfurter;
+﻿using System.Net.Http;
+using Microsoft.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using LRSCard.CurrencyService.Application.Interfaces;
+using LRSCard.CurrencyService.Infrastructure.ExchangeRateProviders.Frankfurter;
+using LRSCard.CurrencyService.Infrastructure.Options;
+using Microsoft.Extensions.Options;
 
 namespace LRSCard.CurrencyService.Infrastructure
 {
@@ -12,12 +17,55 @@ namespace LRSCard.CurrencyService.Infrastructure
         {
             var baseUrl = configuration["ExchangeProvider:Frankfurter:BaseUrl"] ?? "https://api.frankfurter.app";
 
+            var resiliencyConfig = configuration
+                .GetSection("ExchangeProviderResiliency")
+                .Get<ExchangeProviderResiliencyOptions>() ?? new ExchangeProviderResiliencyOptions();
+
             services.AddHttpClient<IExchangeRateProvider, FrankfurterExchangeRateProvider>((provider, client) =>
             {
                 client.BaseAddress = new Uri(baseUrl);
-            });
+            })
+            .AddPolicyHandler(GetRetryPolicy(resiliencyConfig))
+            .AddPolicyHandler(GetCircuitBreakerPolicy(resiliencyConfig)); 
 
             return services;
+        }
+
+        /// This method configures a retry policy using Polly for transient HTTP errors.
+        /// It retries the request up to 3 times with an exponential backoff strategy.(Default values)
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy(ExchangeProviderResiliencyOptions options)
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(
+                    retryCount: options.RetryCount,
+                    sleepDurationProvider: attempt =>
+                        TimeSpan.FromSeconds(Math.Pow(options.InitialBackoffSeconds, attempt)),
+                    onRetry: (outcome, delay, attempt, _) =>
+                    {
+                        Console.WriteLine($"Retry {attempt} after {delay.TotalSeconds}s due to: {outcome.Exception?.Message ?? outcome.Result?.StatusCode.ToString()}");
+                    });
+        }
+
+        /// <summary>
+        /// Add Circuit Breaker policy to the HTTP client.
+        /// 2 attempts before breaking the circuit for 30 seconds(Default values).
+        /// </summary>
+        /// <returns></returns>
+        private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy(ExchangeProviderResiliencyOptions options)
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(
+                    handledEventsAllowedBeforeBreaking: options.CircuitBreakerFailureThreshold,
+                    durationOfBreak: TimeSpan.FromSeconds(options.CircuitBreakerDurationInSeconds),
+                    onBreak: (outcome, delay) =>
+                        Console.WriteLine($"Circuit opened for {delay.TotalSeconds}s due to: {outcome.Exception?.Message ?? outcome.Result?.StatusCode.ToString()}"),
+                    onReset: () =>
+                        Console.WriteLine("Circuit closed."),
+                    onHalfOpen: () =>
+                        Console.WriteLine("Circuit is half-open, testing...")
+                );
         }
     }
 }
---
+++ src/LRSCard.CurrencyService.Infrastructure/Options/ExchangeProviderResiliencyOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRSCard.CurrencyService.Infrastructure.Options
+{
+    public class ExchangeProviderResiliencyOptions
+    {
+        public int RetryCount { get; set; } = 3;
+        public int InitialBackoffSeconds { get; set; } = 2;
+        public int CircuitBreakerFailureThreshold { get; set; } = 2;
+        public int CircuitBreakerDurationInSeconds { get; set; } = 30;
+    }
+}
